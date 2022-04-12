87faa4486d0b5a5bd0721265871d7f962a282f9a
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-04-11 21:50:52 -0400

    loading Screen component

---
+++ components/LoadingScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoadingScreen() {
+  return (
+    <div className="centeredOnScreen">
+      <h1>Loading...</h1>
+    </div>
+  );
+}
+
+export default LoadingScreen;

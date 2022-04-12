c82f0ebeeb40a61de72aff4c5d4acc7c6ac190e7
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-04-12 18:07:55 -0400

    Income, Expense and goals page added

--- components/ledger/SideBar.js
+++ components/ledger/SideBar.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function SideBar() {
   const { data: session } = useSession();
 
+  const router = useRouter();
+
+  console.log(session);
+  const { image, name } = session.user;
   return (
-    <div className="w-[300px] h-screen bg-slate-200">
-      <div className="flex justify-between p-4">
-        <h1>{session.user.name}</h1>
-        <button onClick={signOut}>signOut</button>
+    <div className="w-[300px] h-screen bg-white border-r-2 border-gray-500">
+      <div className="flex items-center p-4 border-b-2 border-gray-500">
+        <img
+          src={image}
+          alt="profile pic"
+          className="rounded-full h-12 cursor-pointer"
+          onClick={() => router.push("/")}
+        />
+        <div className="ml-3">
+          <h1 className="text-xl">{name}</h1>
+          <button onClick={signOut}>sign out</button>
+        </div>
       </div>
 
-      <h1>Income</h1>
-      <h1>Expense</h1>
+      {/* Pages */}
+      <div className="flex flex-col">
+        <button onClick={() => router.push("/income")}>Income</button>
+        <button onClick={() => router.push("/expense")}>Expense</button>
+        <button onClick={() => router.push("/goals")}>Goals</button>
+      </div>
     </div>
   );
 }
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -3,13 +3,19 @@ import { getProviders, signIn } from "next-auth/react";
 export default function SignIn({ providers }) {
   return (
     <>
-      {Object.values(providers)?.map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
+      <div className="centeredOnScreen">
+        <h1>Welcome to Legder</h1>
+        {Object.values(providers)?.map((provider) => (
+          <div key={provider.name}>
+            <button
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              className="bg-blue-500 p-2 rounded-full"
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
